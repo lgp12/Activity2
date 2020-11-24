87fbb01812713d65638e1459036964998dd0d84c
Leesa Marie Price <literaryleesa@gmail.com>
2020-11-24 10:27:13 -0600

    tutorial code

--- index.js
+++ index.js
@@ -9,5 +9,18 @@
 // } catch (error) {
 //     core.setFailed(error.message);
 // }
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-console.log("I am here");
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
