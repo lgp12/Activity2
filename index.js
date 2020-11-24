e4b0972b041b9979e94a8d149d488a5b20af7a50
Leesa Marie Price <literaryleesa@gmail.com>
2020-11-24 10:21:18 -0600

    clean index js file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const core = require('@actions/core');
+// const core = require('@actions/core');
 
-try {
-    let name = core.getInput('yourName');
-    let age = core.getInput('yourAge');
-    let years = core.getInput('additonalYears');
-    let newAge = age + years;
-    console.log("Hello " + name + ", in " + years + ", you wil be " + newAge +"! Won't you feel old?...");
-} catch (error) {
-    core.setFailed(error.message);
-}
+// try {
+//     let name = core.getInput('yourName');
+//     let age = core.getInput('yourAge');
+//     let years = core.getInput('additonalYears');
+//     let newAge = age + years;
+//     console.log("Hello " + name + ", in " + years + ", you wil be " + newAge +"! Won't you feel old?...");
+// } catch (error) {
+//     core.setFailed(error.message);
+// }
+
+console.log("I am here");
