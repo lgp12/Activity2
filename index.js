2beba73c44c47d9f6c3d8cef43896ce7ffbdedc5
Leesa Marie Price <literaryleesa@gmail.com>
2020-12-04 15:21:14 -0600

    label

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const name = String(core.getInput('NAME'));
+    const name = core.getInput('NAME');
     const age = Number(core.getInput('AGE'));
     const years = Number(core.getInput('TIMESPAN'));
     let newAge = age + years;
 
-    console.log("Hello " + name + ", in " + years + " years, you wil be " + newAge +"! Won't you feel old?...");
+    console.log("Hello " + name + ", in " + years + " years, you wil be " + newAge +"! Won't you feel old?....");
 } catch (error) {
   core.setFailed(error.message);
 }
