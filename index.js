49962cd608bb5d2e130c4d0bfe7616cc58f390f1
Leesa Marie Price <literaryleesa@gmail.com>
2020-12-04 15:11:59 -0600

    number

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
     const name = core.getInput('NAME');
     const age = Number(core.getInput('AGE'));
-    const years = core.getInput('TIMESPAN');
+    const years = Number(core.getInput('TIMESPAN'));
     let newAge = age + years;
 
     console.log("Hello " + name + ", in " + years + " years, you wil be " + newAge +"! Won't you feel old?...");
