23bc05166a3b6c55d3a6610356be3c5af3fb6067
Leesa Marie Price <literaryleesa@gmail.com>
2020-12-04 14:49:11 -0600

    SECRETS

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// const core = require('@actions/core');
-
-// try {
-//     let name = core.getInput('yourName');
-//     let age = core.getInput('yourAge');
-//     let years = core.getInput('additonalYears');
-//     let newAge = age + years;
-//     console.log("Hello " + name + ", in " + years + ", you wil be " + newAge +"! Won't you feel old?...");
-// } catch (error) {
-//     core.setFailed(error.message);
-// }
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -16,16 +5,10 @@ try {
     const name = core.getInput('NAME');
     const age = core.getInput('AGE');
     const years = core.getInput('TIMESPAN');
+    console.log(name);
+    console.log(age);
     let newAge = age +years;
     console.log("Hello " + name + ", in " + years + ", you wil be " + newAge +"! Won't you feel old?...");
-//   // `who-to-greet` input defined in action metadata file
-//   const nameToGreet = core.getInput('who-to-greet');
-//   console.log(`Hello ${nameToGreet}!`);
-//   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
